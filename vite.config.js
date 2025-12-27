2a19658d62e3570beeb17155f8b6ba1253897d99
Cristian-Aguilera-316 <cristian.ap316@gmail.com>
2025-12-26 23:16:17 -0300

    build 2 para github

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/Desafiolatam_M4D4/",
-  build: {
-    outDir: "docs",
-    emptyOutDir: true
-  }
+  plugins: [react()]
 });
