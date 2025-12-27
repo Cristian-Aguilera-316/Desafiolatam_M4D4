c90552d834d9698c5a6cf8c31a3defc940cd17ea
Cristian-Aguilera-316 <158117506+Cristian-Aguilera-316@users.noreply.github.com>
2025-12-26 22:23:13 -0300

    build para github

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Desafiolatam_M4D4/',
+  base: "/Desafiolatam_M4D4/",
   build: {
-    outDir: 'docs'
+    outDir: "docs",
+    emptyOutDir: true
   }
-})
+});
